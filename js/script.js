7e2fd91e00cc0488b323deddc5202853147d762c
Celsio Capapelom <ccelsio@gmail.com>
2022-10-17 17:20:37 +0100

    Custom select SINGLE added to components

--- js/script.js
+++ js/script.js
@@ -49,107 +49,235 @@ dropdownCollection.forEach(dropdown => {
 });
 
 /////////////////////////////////////////////////
-// Select Menus
+// Custom Select
 
-// a) Functions
-const toggleVisibility = (element) =>
-  element.classList.contains("select__list--hidden")
-    ? element.classList.remove("select__list--hidden")
-    : element.classList.add("select__list--hidden");
+// Selections, variables
+const selectCollection = document.querySelectorAll('.select--custom');
 
-// b) Selections
-const selectNativeCollection =
-  document.getElementsByClassName("select--native");
-const selectListCollection =
-  document.getElementsByClassName("select__list");
-const selectActiveCollection =
-  document.getElementsByClassName("select--active");
+// Functions
 
-Array.prototype.forEach.call(selectNativeCollection, (s, i) => {
-  // 1) Insert selectActive and selectList elements
-  s.insertAdjacentHTML(
-    "beforebegin",
-    `
-    <div class="select--container u-border">
-      <div class="select--active u-vertical-center u-gap-s">
-        <img class="u-circle" src="images/profile.png" alt="Item image">  
-        <span class="u-vertical-center"></span>
-        <i class="dropdown__icon las la-angle-right"></i>
-      </div>
-      <div class="select__list select__list--hidden u-border"></div> 
-    </div>
-  `
-  );
+selectCollection.forEach(select => {
+  // Selections, variables
+  const activeItem = select.querySelector(".select__btn");
+  const selectList = select.querySelector(".select__list");
+  let isOpenedList = false;
+  const activeItemIcon = 
+    activeItem.querySelector(".select--custom__icon");    
+  const itemsCollection = select.querySelectorAll('.select__item');
+  
+  const closeList = () => {
+    selectList.classList.add("select__list--hidden");
+    isOpenedList = false;
+  };
 
-  // 2) Add items to selectList
-  const optionTextArray = Array.prototype.map.call(
-    s.options,
-    (o) => o.innerHTML
-  );
+  const openList = () => {
+    selectList.classList.remove("select__list--hidden");
+    isOpenedList = true;
+  };
 
-  let html = "";
-  optionTextArray.forEach(
-    (t) =>
-      (html = html.concat(
-        `<div class="select__item u-vertical-center u-gap-s">
-          <img class="u-circle" src="images/profile.png" alt="Item image"> 
-          <span class="u-vertical-center">${t}</span>
-          <i class="dropdown__icon"></i>
-        </div>`
-      ))
-  );
+  const toggleList = () =>
+  isOpenedList ? closeList() : openList();
 
-  const selectList = s.previousElementSibling.querySelector('.select__list');
-  selectList.innerHTML = html;
+  // Event Listeners
+   
+  // Open or close the list by clicking on active item
+  activeItem.addEventListener("click", (e) => {
+    const icon = activeItem.querySelector(".select--custom__icon");
 
-  // 3) Add initial HTML to selectActive's span
-  selectList.previousElementSibling.querySelector('span').innerHTML = 
-    `${s.options[s.options.selectedIndex].innerHTML}`;
+    // Rotate icon when activeIcon is clicked
+    icon.classList.contains("select--custom__icon--rotate")
+      ? icon.classList.remove("select--custom__icon--rotate")
+      : icon.classList.add("select--custom__icon--rotate");
 
-  // 4) Update selectActive and selectNative WHEN selectItem is clicked
-  const selectItemsCollection = selectList.querySelectorAll(".select__item");
-  const selectActive = selectList.previousElementSibling;
-  selectItemsCollection.forEach((item, index, arr) => {
-    item.addEventListener("click", () => {
-      // Add select--active class on clicked item
-      arr.forEach((p) => {
-        p.classList.remove("select--active");
-        p.querySelector('.dropdown__icon').classList.remove('las', 'la-check');
-      });
-      item.classList.add("select--active");
-      item.querySelector(".dropdown__icon").classList.add("las", "la-check");
+      toggleList();
+      
+  });
 
-      // Update selectActive's InnerHTML
-      selectActive.removeChild(selectActive.querySelector("img"));
-      selectActive.removeChild(selectActive.querySelector("span"));
-      selectActive.insertAdjacentHTML(
-        "afterbegin",
-        `
-        <img class="u-circle" src="images/profile.png" alt="Item image">  
-        <span class="u-vertical-center">${item.querySelector("span").innerHTML}</span>
-      `
-      );
-      selectActive
-        .querySelector("i")
-        .classList.remove("dropdown__icon--rotate");
+  // Close List clicking outside of select
+  document.addEventListener("click", (e) => {
+    const isClickOutsideSelect = !e.target.closest(".select--custom");
+    if (isClickOutsideSelect && isOpenedList) closeList();
+  });
 
-      // Update selectedOption on selectNative
-      s.options.selectedIndex = index;
+  // // Close List by pressing the ESC key
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && isOpenedList) closeList();
+  });
 
-      // Close selectList
-      toggleVisibility(selectListCollection[i]);
+  // Handle click on items
+  itemsCollection.forEach(item => {
+    item.addEventListener('click', (e) => {
+      console.log(e.target);
+      // 1) Remove class and atribute related to active on all item
+      if ( !item.parentElement.getAttribute('aria-multiselectable') ) {
+        itemsCollection.forEach(i => {
+          i.setAttribute('aria-selected', false);
+          i.classList.remove('select__item--active');
+        });
+      }
+      // 2) Add class and atribute related to active on clicked item
+      item.classList.add('select__item--active');
+      item.setAttribute('aria-selected', true);
+
+      //3) Update the aria-activedescendant value on the listbox 
+      selectList.setAttribute('aria-activedescendant', item.getAttribute('id'))
+
+      // 4) Update activeItem and close list
+      
+      // Rotate icon when activeIcon is clicked
+      const icon = activeItem.querySelector(".select--custom__icon");
+      icon.classList.contains("select--custom__icon--rotate")
+      ? icon.classList.remove("select--custom__icon--rotate")
+      : icon.classList.add("select--custom__icon--rotate");
+      
+      // Update img and span
+      activeItem.querySelector('span').innerText = item.querySelector('span').innerText
+      activeItem.querySelector('img').setAttribute('src', item.querySelector('img').getAttribute('src')); 
+
+      // toggleList(); 
+      if (!item.parentElement.getAttribute('aria-multiselectable')) closeList();
     });
   });
 
-  // 5) Rotate icon when selectActive is clicked
-  selectActive.addEventListener("click", (e) => {
-    const icon = e.target.parentNode.querySelector("i");
-    icon.classList.contains("dropdown__icon--rotate")
-      ? icon.classList.remove("dropdown__icon--rotate")
-      : icon.classList.add("dropdown__icon--rotate");
-    toggleVisibility(e.target.parentNode.nextElementSibling);
-  });
-});
+   // Handle 'Space bar' and 'Enter' press
+   itemsCollection.forEach(item => {
+    item.onkeydown = (e) => {
+      // console.log(e.target, e.key);
+      // if (e.key !== 'Enter') return;
+      if (e.key !== ' ' && e.key !== 'Enter') return;
+      console.log(e.target, e.key);
+      // 1) Remove class and atribute related to active on all item
+      itemsCollection.forEach(i => {
+        i.setAttribute('aria-selected', false);
+        i.classList.remove('select__item--active');
+      });
+
+      // 2) Add class and atribute related to active on clicked item
+      item.classList.add('select__item--active');
+      item.setAttribute('aria-selected', true);
+
+      //3) Update the aria-activedescendant value on the listbox 
+      selectList.setAttribute('aria-activedescendant', item.getAttribute('id'))
+
+      // 4) Update activeItem and close list
+      
+      // Rotate icon when activeIcon is clicked
+      const icon = activeItem.querySelector(".select--custom__icon");
+      icon.classList.contains("select--custom__icon--rotate")
+      ? icon.classList.remove("select--custom__icon--rotate")
+      : icon.classList.add("select--custom__icon--rotate");
+      
+      // Update img and span
+      activeItem.querySelector('span').innerText = item.querySelector('span').innerText
+      activeItem.querySelector('img').setAttribute('src', item.querySelector('img').getAttribute('src')); 
+
+      toggleList(); 
+    }
+   });
+})
+
+// /////////////////////////////////////////////////
+// // Select Menus
+
+// // a) Functions
+// const toggleVisibility = (element) =>
+//   element.classList.contains("select__list--hidden")
+//     ? element.classList.remove("select__list--hidden")
+//     : element.classList.add("select__list--hidden");
+
+// // b) Selections
+// const selectNativeCollection =
+//   document.getElementsByClassName("select--native");
+// const selectListCollection =
+//   document.getElementsByClassName("select__list");
+// const selectActiveCollection =
+//   document.getElementsByClassName("select--active");
+
+// Array.prototype.forEach.call(selectNativeCollection, (s, i) => {
+//   // 1) Insert selectActive and selectList elements
+//   s.insertAdjacentHTML(
+//     "beforebegin",
+//     `
+//     <div class="select--container u-border">
+//       <div class="select--active u-vertical-center u-gap-s">
+//         <img class="u-circle" src="images/profile.png" alt="Item image">  
+//         <span class="u-vertical-center"></span>
+//         <i class="dropdown__icon las la-angle-right"></i>
+//       </div>
+//       <div class="select__list select__list--hidden u-border"></div> 
+//     </div>
+//   `
+//   );
+
+//   // 2) Add items to selectList
+//   const optionTextArray = Array.prototype.map.call(
+//     s.options,
+//     (o) => o.innerHTML
+//   );
+
+//   let html = "";
+//   optionTextArray.forEach(
+//     (t) =>
+//       (html = html.concat(
+//         `<div class="select__item u-vertical-center u-gap-s">
+//           <img class="u-circle" src="images/profile.png" alt="Item image"> 
+//           <span class="u-vertical-center">${t}</span>
+//           <i class="dropdown__icon"></i>
+//         </div>`
+//       ))
+//   );
+
+//   const selectList = s.previousElementSibling.querySelector('.select__list');
+//   selectList.innerHTML = html;
+
+//   // 3) Add initial HTML to selectActive's span
+//   selectList.previousElementSibling.querySelector('span').innerHTML = 
+//     `${s.options[s.options.selectedIndex].innerHTML}`;
+
+//   // 4) Update selectActive and selectNative WHEN selectItem is clicked
+//   const selectItemsCollection = selectList.querySelectorAll(".select__item");
+//   const selectActive = selectList.previousElementSibling;
+//   selectItemsCollection.forEach((item, index, arr) => {
+//     item.addEventListener("click", () => {
+//       // Add select--active class on clicked item
+//       arr.forEach((p) => {
+//         p.classList.remove("select--active");
+//         p.querySelector('.dropdown__icon').classList.remove('las', 'la-check');
+//       });
+//       item.classList.add("select--active");
+//       item.querySelector(".dropdown__icon").classList.add("las", "la-check");
+
+//       // Update selectActive's InnerHTML
+//       selectActive.removeChild(selectActive.querySelector("img"));
+//       selectActive.removeChild(selectActive.querySelector("span"));
+//       selectActive.insertAdjacentHTML(
+//         "afterbegin",
+//         `
+//         <img class="u-circle" src="images/profile.png" alt="Item image">  
+//         <span class="u-vertical-center">${item.querySelector("span").innerHTML}</span>
+//       `
+//       );
+//       selectActive
+//         .querySelector("i")
+//         .classList.remove("dropdown__icon--rotate");
+
+//       // Update selectedOption on selectNative
+//       s.options.selectedIndex = index;
+
+//       // Close selectList
+//       toggleVisibility(selectListCollection[i]);
+//     });
+//   });
+
+//   // 5) Rotate icon when selectActive is clicked
+//   selectActive.addEventListener("click", (e) => {
+//     const icon = e.target.parentNode.querySelector("i");
+//     icon.classList.contains("dropdown__icon--rotate")
+//       ? icon.classList.remove("dropdown__icon--rotate")
+//       : icon.classList.add("dropdown__icon--rotate");
+//     toggleVisibility(e.target.parentNode.nextElementSibling);
+//   });
+// });
 
 console.log("SCRIPT_END");
 
